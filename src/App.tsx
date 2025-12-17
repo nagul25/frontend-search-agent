6e941d944f9eb0c0f7a2cebfde353d2d72fa6156
Aravind <aravind.babu@numentica-ui.com>
2025-12-17 17:23:51 +0530

    Enabled assessment bot route

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import styles from './styles/App.module.css';
-import {AssessmentBot, LookupBot} from './components';
+import { AssessmentBot, LookupBot } from './components';
 
 const App: React.FC = () => {
   return (
     <Router>
       <div className={styles.app}>
         <header className={styles.header}>
-            <h1 className={styles.headerTitle}>ARB Assistant Platform</h1>
-            <div className={styles.actionBtns}>
-              <NavLink to="/"
+          <h1 className={styles.headerTitle}>ARB Assistant Platform</h1>
+          <div className={styles.actionBtns}>
+            <NavLink to="/"
               className={({ isActive }) => isActive ? styles.activeLink : ''}
-               style={{ textDecoration: 'none', color: 'white', marginRight: '12px' }}>
-                Lookup Bot
-              </NavLink>
-              {/* <NavLink to="/assessment"
-              className={({isActive}) => isActive ? styles.activeLink : ''}
+              style={{ textDecoration: 'none', color: 'white', marginRight: '12px' }}>
+              Lookup Bot
+            </NavLink>
+            <NavLink to="/assessment"
+              className={({ isActive }) => isActive ? styles.activeLink : ''}
               style={{ textDecoration: 'none', color: 'white' }}>
-                Assessment Bot
-              </NavLink> */}
-            </div>
+              Assessment Bot
+            </NavLink>
+          </div>
         </header>
         <Routes>
           <Route path="/" element={<LookupBot />} />
